f4255dfa1e57cc922520085b08597776836d0382
Marie M <29389592+MarieIndustries@users.noreply.github.com>
2023-04-16 00:50:56 -0400

    cleaning up header and adding an image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../index.css'
+import bowl from '../images/steamy-bowl.png'
 
 export default function Header(){
     return(
         <div className="header">
-            <h1>Carb Cards</h1>
-            <p>What are you cooking tonight?</p>
+            <img src={bowl} className="header-img" />
+            <h1 className="header--title">Carb Cards</h1>
+            <h4 className="header--subtitle">What are you cooking tonight?</h4>
         </div>
     )
 }
