886c577831c0087d361206f6ab193420497e37fe
Marie M <29389592+MarieIndustries@users.noreply.github.com>
2023-04-16 00:50:57 -0400

    Organizing Nutrition Info into table, and other formatting adjustments for readability

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,11 @@ export default function Card(props){
 
     const data = CarbData.data;
 
-    //TODO: format nutrition information into table
-    
+    //todo: add fancy font https://fonts.google.com/specimen/Andika?query=Andika+
+
     const directionsArray = data[props.recipeIndex].directions.split('\n')
     var directionsRows = [];
+    directionsRows.push(<div>Cooking Instructions:</div>)
     directionsArray.forEach(instruction => directionsRows.push(<div>{instruction}</div>))
 
     const showMacros = props.showMacros;
@@ -18,23 +19,34 @@ export default function Card(props){
         return(
             <div>
                 <div>Nutrition Information Per 100g</div>
-                <div>Calories: {nutrition.calories}</div>
-                <div>Carbs: {nutrition.macros.carbs}</div>
-                <div>Protein: {nutrition.macros.protein}</div>
-                <div>Fat: {nutrition.macros.fat}</div>
-                <div>Fiber: {nutrition.fiber}</div>
+                <table className="nutritionTable">
+                    <tr>
+                        <th>Calories</th>
+                        <th>Carbs</th>
+                        <th>Protein</th>
+                        <th>Fat</th>
+                        <th>Fiber</th>
+                    </tr>
+                    <tr>
+                        <td>{nutrition.calories}</td>
+                        <td>{nutrition.macros.carbs}</td>
+                        <td>{nutrition.macros.protein}</td>
+                        <td>{nutrition.macros.fat}</td>
+                        <td>{nutrition.fiber}</td>
+                    </tr>
+                </table>
             </div>
         )
     } 
     
-
     return(
         <div className="parentCard">
            <div className="card">
-                 <h3>{data[props.recipeIndex].title}</h3>
+                 <h3 className="cardTitle">{data[props.recipeIndex].title}</h3>
                  {directionsRows}
+                 <br />
                  <button onClick={props.handleShowMacros}>Toggle Nutrition Information</button>
-                {showMacros && <div>{NutritionDisplay()}</div>}  
+                 {showMacros && <div>{NutritionDisplay()}</div>}  
              </div>
         </div>
     )
